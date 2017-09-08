b1022706fefb58bd8ac9d4e3f15922ef5b2ac272
Chris Beer <cabeer@stanford.edu>
2017-09-08 08:46:32 -0700

    Update StacksFileResolver (and other routing infrastructure) to support ids that are the encoded path to the image relative to the base path

--- test/integration/riiif_test.rb
+++ test/integration/riiif_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class RiiifTest < ActionDispatch::IntegrationTest
   test 'can return image info' do
-    get '/image-service/dz302gz2129/info.json'
+    get '/image-service/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.jp2/info.json'
     assert_response :ok
     assert_equal('{"width":6501,"height":4381,'\
                   '"@context":"http://iiif.io/api/image/2/context.json",' \
-                  '"@id":"http://www.example.com/image-service/dz302gz2129",'\
+                  '"@id":"http://www.example.com/image-service/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.jp2",'\
                   '"protocol":"http://iiif.io/api/image",'\
                   '"profile":["http://iiif.io/api/image/2/level1.json",'\
                   '{"formats":["jpg","png"]}]}', response.parsed_body)
   end
 
   test 'can return an image' do
-    get '/image-service/dz302gz2129/full/!150,300/0/default.jpg'
+    get '/image-service/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.jp2/full/!150,300/0/default.jpg'
     assert_response :ok
 
     Tempfile.open('test', encoding: 'ascii-8bit') do |f|
@@ -22,6 +22,18 @@ class RiiifTest < ActionDispatch::IntegrationTest
       _, format, dimensions, _ = `identify #{f.path}`.split(/\s+/)
       assert_equal 'JPEG', format
       assert_equal '150x101', dimensions
+    end
+  end
+
+  test 'requires an appropriate image extension' do
+    assert_raises ArgumentError do
+      get '/image-service/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.txt/full/!150,300/0/default.jpg'
+    end
+  end
+
+  test 'prevents directory traversal' do
+    assert_raises ArgumentError do
+      get '/image-service/..%2F..%2Fetc%2Fpasswd.jp2/full/!150,300/0/default.jpg'
     end
   end
 end
