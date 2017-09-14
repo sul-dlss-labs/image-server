ddeb4af66284bf0ec9a758fd4ac974488f30dfac
Chris Beer <cabeer@stanford.edu>
2017-09-14 10:31:41 -0700

    Update routing to use the prefix /iiif/2

--- test/integration/riiif_test.rb
+++ test/integration/riiif_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class RiiifTest < ActionDispatch::IntegrationTest
   test 'can return image info' do
-    get '/image-service/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.jp2/info.json'
+    get '/iiif/2/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.jp2/info.json'
     assert_response :ok
     assert_equal('{"width":6501,"height":4381,'\
                   '"@context":"http://iiif.io/api/image/2/context.json",' \
-                  '"@id":"http://www.example.com/image-service/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.jp2",'\
+                  '"@id":"http://www.example.com/iiif/2/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.jp2",'\
                   '"protocol":"http://iiif.io/api/image",'\
                   '"profile":["http://iiif.io/api/image/2/level1.json",'\
                   '{"formats":["jpg","png"]}]}', response.parsed_body)
   end
 
   test 'can return an image' do
-    get '/image-service/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.jp2/full/!150,300/0/default.jpg'
+    get '/iiif/2/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.jp2/full/!150,300/0/default.jpg'
     assert_response :ok
 
     Tempfile.open('test', encoding: 'ascii-8bit') do |f|
@@ -27,13 +27,13 @@ class RiiifTest < ActionDispatch::IntegrationTest
 
   test 'requires an appropriate image extension' do
     assert_raises ArgumentError do
-      get '/image-service/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.txt/full/!150,300/0/default.jpg'
+      get '/iiif/2/dz%2F302%2Fgz%2F2129%2FPC0082_b06_f01_Baseball_1986_0012.txt/full/!150,300/0/default.jpg'
     end
   end
 
   test 'prevents directory traversal' do
     assert_raises ArgumentError do
-      get '/image-service/..%2F..%2Fetc%2Fpasswd.jp2/full/!150,300/0/default.jpg'
+      get '/iiif/2/..%2F..%2Fetc%2Fpasswd.jp2/full/!150,300/0/default.jpg'
     end
   end
 end
